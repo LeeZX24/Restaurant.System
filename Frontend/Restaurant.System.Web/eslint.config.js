fa23c21f4a3b36689b402c8aab7e09636db3b4b8
LeeZX24 <leezixiang24@gmail.com>
2025-12-12 09:22:55 +0000

    restructure

--- Frontend/Restaurant.System.Web/eslint.config.js
+++ Frontend/Restaurant.System.Web/eslint.config.js
@@ -3,41 +3,40 @@ const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
-module.exports = tseslint.config(
-  {
-    files: ['src/**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
+module.exports =
+{
+  files: ['src/**/*.ts'],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    ...angular.configs.tsRecommended,
+  ],
+  processor: angular.processInlineTemplates,
+  rules: {
+    "@angular-eslint/directive-selector": [
+      "error",
+      {
+        type: "attribute",
+        prefix: ["app", "rs"],
+        style: "camelCase",
+      },
     ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: ["app", "rs"],
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: ["app", "rs"],
-          style: "kebab-case",
-        },
-      ],
-    },
+    "@angular-eslint/component-selector": [
+      "error",
+      {
+        type: "element",
+        prefix: ["app", "rs"],
+        style: "kebab-case",
+      },
+    ],
   },
-  {
-    files: ['src/**/*.html'],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
-);
+},
+{
+  files: ['src/**/*.html'],
+  extends: [
+    ...angular.configs.templateRecommended,
+    ...angular.configs.templateAccessibility,
+  ],
+  rules: {},
+}
