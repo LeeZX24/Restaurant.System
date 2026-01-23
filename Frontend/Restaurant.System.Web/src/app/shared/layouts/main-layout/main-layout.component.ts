205d8738458b07fe62a2c9b497767680fdddead9
LeeZX24 <leezixiang24@gmail.com>
2026-01-23 09:14:43 +0000

    fix: initial versioning

--- Frontend/Restaurant.System.Web/src/app/shared/layouts/main-layout/main-layout.component.ts
+++ Frontend/Restaurant.System.Web/src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import packageInfo from '../../../../../package.json';
 
 @Component({
   selector: 'rs-main-layout',
@@ -8,4 +9,5 @@ import { RouterOutlet } from "@angular/router";
   styleUrl: './main-layout.component.css',
 })
 export class MainLayoutComponent {
+  version = 'v' + packageInfo.version;
 }
